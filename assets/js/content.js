f64faf91910e398b3c37f615c8fafc9786ddb844
Kevin Lawrenze S. Lapuz <120726382+KevKujo23@users.noreply.github.com>
2026-02-26 22:08:38 +0800

    Update content.js

--- assets/js/content.js
+++ assets/js/content.js
@@ -87,7 +87,7 @@ window.PORTFOLIO_CONTENT = {
         "Lead-generation and marketing WordPress website for Delloro Saulog Law Offices, built to present firm information clearly and support appointment inquiries.",
       tech: ["WordPress", "PHP", "JavaScript", "HTML", "CSS", "WooCommerce", "Bootstrap", "SEO"],
       repoUrl: "",
-      liveUrl: "",
+      liveUrl: "https://staging-7ee1-dellorosauloglaw.wpcomstaging.com/",
       image: "assets/img/DelloroWordpress.png",
       status: "Finished (Pending Launch)",
       highlights: [
